65fca587bccb8d06fdd6cc57955aba657125b4a4
mpwork <maarten.van.der.peet@di.huc.knaw.nl>
2019-10-18 17:29:07 +0200

    fixed irritating docker apache notice

--- webroot/server.php
+++ webroot/server.php
@@ -1,7 +1,9 @@
 <?php
 
-// $file = file_get_contents("json_examples/timbuctoo_edit_metadata.json");
-$file = file_get_contents("json_examples/timbuctoo_edit_metadata_dev.json");
+$file = file_get_contents("json_examples/timbuctoo_edit_metadata.json");
+
+// uncomment the line below for a .json file with new features, in development
+// $file = file_get_contents("json_examples/timbuctoo_edit_metadata_dev.json");
 
 header('Content-Type: application/json');
 header("Access-Control-Allow-Origin: *");
