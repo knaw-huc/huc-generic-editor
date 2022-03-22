a798c76af4df5e4a32aca9b1bd5698e562c53d59
Rob Zeeman <rob.zeeman@di.huc.knaw.nl>
2022-03-22 18:17:03 +0100

    Bug fixed (hiding fields)

--- js/src/ccfparser.js
+++ js/src/ccfparser.js
@@ -18,7 +18,7 @@ var formBuilder = {
         createAutoCompletes();
         if (obj.record !== undefined) {
             fillValues(obj.record);
-            $(".disabledElement").attr("class", "element");
+            $(".disabledElement").addClass("element").removeClass("disabledElement");
             $(".disabledComponent").addClass("component").removeClass("disabledComponent");
             $(".optionalCompBtn").click();
         }
@@ -164,8 +164,9 @@ var formBuilder = {
         input.appendChild(Msg);
         html.appendChild(input);
         if (element.attributes.hide === 'yes') {
-            $(html).addClass("hidden_element");
-            console.log('hide');
+            $(html).addClass("blocked_element");
+            console.log(element.attributes.name);
+            console.log($(html).attr("class"));
         }
         $("#" + componentID).append(html);
         validationProfiles[element.ID] = element;
@@ -326,6 +327,7 @@ var formBuilder = {
                     control.appendChild(option);
                     break;
                 case 'string':
+                case 'anyURI':
                 case 'decimal':
                     var control = this.createTextInputField(element);
                     break;
@@ -337,7 +339,7 @@ var formBuilder = {
                     if (element.attributes.autoValue === undefined) {
                         control.setAttribute('value', "");
                     } else {
-                        control.setAttribute('value', element.attributes.autoValue.value);
+                        control.setAttribute('value', element.attributes.autoValue);
                     }
 
             }
@@ -382,6 +384,7 @@ var formBuilder = {
                 control.setAttribute('id', element.ID);
                 break;
             case 'single':
+            case 'anyURI':
             default:
                 var control = document.createElement('input');
                 control.setAttribute('type', 'text');
@@ -390,7 +393,7 @@ var formBuilder = {
                 if (element.attributes.autoValue === undefined) {
                     control.setAttribute('value', "");
                 } else {
-                    control.setAttribute('value', element.attributes.autoValue.value);
+                    control.setAttribute('value', element.attributes.autoValue);
                 }
                 control.setAttribute('id', element.ID);
                 if (element.attributes.autoCompleteURI !== undefined) {
@@ -453,14 +456,16 @@ var formBuilder = {
     createButtons: function () {
         var buttonFrame = document.createElement('div');
         buttonFrame.setAttribute('id', 'btnFrame');
-        var control = document.createElement('input');
+
+        /*var control = document.createElement('input');
         control.setAttribute('type', 'button');
         control.setAttribute('value', ccfOptions.submitButton.label);
         control.setAttribute('id', 'OKbtn');
         control.onclick = function () {
             validate();
         };
-        buttonFrame.appendChild(control);
+        buttonFrame.appendChild(control);*/
+
         var control = document.createElement('input');
         control.setAttribute('type', 'button');
         control.setAttribute('value', ccfOptions.saveButton.label);
@@ -469,6 +474,7 @@ var formBuilder = {
             sendForm();
         };
         buttonFrame.appendChild(control);
+
         if (ccfOptions.resetButton !== null && ccfOptions.resetButton !== undefined) {
             var control = document.createElement('input');
             control.setAttribute('type', 'button');
@@ -484,7 +490,8 @@ var formBuilder = {
         buttonFrame.appendChild(errorSpace);
         $("#ccform").append(buttonFrame);
     },
-    languages: ['aa', 'ab', 'ae', 'af', 'ak', 'am', 'an', 'ar', 'as', 'av', 'ay', 'az', 'ba', 'be', 'bg', 'bh', 'bi', 'bm', 'bn', 'bo', 'br', 'bs', 'ca', 'ce', 'ch', 'co', 'cr', 'cs', 'cu', 'cv', 'cy', 'da', 'de', 'dv', 'dz', 'ee', 'el', 'en', 'eo', 'es', 'et', 'eu', 'fa', 'ff', 'fi', 'fj', 'fo', 'fr', 'fy', 'ga', 'gd', 'gl', 'gn', 'gu', 'gv', 'ha', 'he', 'hi', 'ho', 'hr', 'ht', 'hu', 'hy', 'hz', 'ia', 'id', 'ie', 'ig', 'ii', 'ik', 'io', 'is', 'it', 'iu', 'ja', 'jv', 'ka', 'kg', 'ki', 'kj', 'kk', 'kl', 'km', 'kn', 'ko', 'kr', 'ks', 'ku', 'kv', 'kw', 'ky', 'la', 'lb', 'lg', 'li', 'ln', 'lo', 'lt', 'lu', 'lv', 'mg', 'mh', 'mi', 'mk', 'ml', 'mn', 'mr', 'ms', 'mt', 'my', 'na', 'nb', 'nd', 'ne', 'ng', 'nl', 'nn', 'no', 'nr', 'nv', 'ny', 'oc', 'oj', 'om', 'or', 'os', 'pa', 'pi', 'pl', 'ps', 'pt', 'qu', 'rm', 'rn', 'ro', 'ru', 'rw', 'sa', 'sc', 'sd', 'se', 'sg', 'si', 'sk', 'sl', 'sm', 'sn', 'so', 'sq', 'sr', 'ss', 'st', 'su', 'sv', 'sw', 'ta', 'te', 'tg', 'th', 'ti', 'tk', 'tl', 'tn', 'to', 'tr', 'ts', 'tt', 'tw', 'ty', 'ug', 'uk', 'ur', 'uz', 've', 'vi', 'vo', 'wa', 'wo', 'xh', 'yi', 'yo', 'za', 'zh', 'zu'],
+    //languages: ['aa', 'ab', 'ae', 'af', 'ak', 'am', 'an', 'ar', 'as', 'av', 'ay', 'az', 'ba', 'be', 'bg', 'bh', 'bi', 'bm', 'bn', 'bo', 'br', 'bs', 'ca', 'ce', 'ch', 'co', 'cr', 'cs', 'cu', 'cv', 'cy', 'da', 'de', 'dv', 'dz', 'ee', 'el', 'en', 'eo', 'es', 'et', 'eu', 'fa', 'ff', 'fi', 'fj', 'fo', 'fr', 'fy', 'ga', 'gd', 'gl', 'gn', 'gu', 'gv', 'ha', 'he', 'hi', 'ho', 'hr', 'ht', 'hu', 'hy', 'hz', 'ia', 'id', 'ie', 'ig', 'ii', 'ik', 'io', 'is', 'it', 'iu', 'ja', 'jv', 'ka', 'kg', 'ki', 'kj', 'kk', 'kl', 'km', 'kn', 'ko', 'kr', 'ks', 'ku', 'kv', 'kw', 'ky', 'la', 'lb', 'lg', 'li', 'ln', 'lo', 'lt', 'lu', 'lv', 'mg', 'mh', 'mi', 'mk', 'ml', 'mn', 'mr', 'ms', 'mt', 'my', 'na', 'nb', 'nd', 'ne', 'ng', 'nl', 'nn', 'no', 'nr', 'nv', 'ny', 'oc', 'oj', 'om', 'or', 'os', 'pa', 'pi', 'pl', 'ps', 'pt', 'qu', 'rm', 'rn', 'ro', 'ru', 'rw', 'sa', 'sc', 'sd', 'se', 'sg', 'si', 'sk', 'sl', 'sm', 'sn', 'so', 'sq', 'sr', 'ss', 'st', 'su', 'sv', 'sw', 'ta', 'te', 'tg', 'th', 'ti', 'tk', 'tl', 'tn', 'to', 'tr', 'ts', 'tt', 'tw', 'ty', 'ug', 'uk', 'ur', 'uz', 've', 'vi', 'vo', 'wa', 'wo', 'xh', 'yi', 'yo', 'za', 'zh', 'zu'],
+    languages: ['ara', 'eng', 'fra', 'fry', 'gla', 'deu', 'grc', 'heb', 'isl', 'gle', 'ita', 'lat', 'yid', 'gml', 'mis', 'nld', 'oci', 'pro', 'spa', 'und'],
     controlHash: []
 };
 
