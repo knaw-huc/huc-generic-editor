32f77bde82b0cab4a7e24c87371a1dc5c17602ee
veraerykalovadi <vera.erykalova@di.huc.knaw.nl>
2026-08-05 09:53:10 +0200

    user table

---
+++ src/components/UserModalForm.tsx
@@ -0,0 +1,122 @@
+import {
+  Dialog,
+  Heading,
+  Form,
+  TextField,
+  Label,
+  Input,
+  Button,
+} from "react-aria-components";
+
+import { Modal } from "./Modal";
+
+interface UserFormData {
+  name: string;
+  role: string;
+  eppn: string;
+  idp: string;
+  edupersontargetedid: string;
+}
+
+interface UserFormModalProps {
+  title: string;
+  user: UserFormData;
+  setUser: React.Dispatch<React.SetStateAction<UserFormData>>;
+  onSave: () => void;
+}
+
+
+export function UserFormModal({
+  title,
+  user,
+  setUser,
+  onSave,
+}: UserFormModalProps) {
+  return (
+    <Modal>
+      <Dialog>
+        <Heading slot="title">{title}</Heading>
+
+        <Form>
+          <TextField isRequired autoFocus>
+            <Label>Name</Label>
+            <Input
+              value={user.name}
+              onChange={(e) =>
+                setUser((prev) => ({
+                  ...prev,
+                  name: e.target.value,
+                }))
+              }
+            />
+          </TextField>
+
+          <TextField>
+            <Label>Role</Label>
+            <Input
+              value={user.role}
+              onChange={(e) =>
+                setUser((prev) => ({
+                  ...prev,
+                  role: e.target.value,
+                }))
+              }
+            />
+          </TextField>
+
+          <TextField>
+            <Label>eppn</Label>
+            <Input
+              value={user.eppn}
+              onChange={(e) =>
+                setUser((prev) => ({
+                  ...prev,
+                  eppn: e.target.value,
+                }))
+              }
+            />
+          </TextField>
+
+          <TextField>
+            <Label>idp</Label>
+            <Input
+              value={user.idp}
+              onChange={(e) =>
+                setUser((prev) => ({
+                  ...prev,
+                  idp: e.target.value,
+                }))
+              }
+            />
+          </TextField>
+
+          <TextField>
+            <Label>edupersontargetedid</Label>
+            <Input
+              value={user.edupersontargetedid}
+              onChange={(e) =>
+                setUser((prev) => ({
+                  ...prev,
+                  edupersontargetedid: e.target.value,
+                }))
+              }
+            />
+          </TextField>
+
+          <div
+            style={{
+              display: "flex",
+              gap: 8,
+              justifyContent: "flex-end",
+            }}
+          >
+            <Button slot="close">Cancel</Button>
+            <Button slot="close" onPress={onSave}>
+              Done
+            </Button>
+          </div>
+        </Form>
+      </Dialog>
+    </Modal>
+  );
+}
--- src/routes/users.tsx
+++ src/routes/users.tsx
@@ -8,8 +8,15 @@ import {
   TableBody,
   TableHeader,
   Button,
+  DialogTrigger,
+
 } from "react-aria-components";
+
 import {createFileRoute} from "@tanstack/react-router";
+import { UserFormModal } from "../components/UserModalForm";
+
+
+
 
 interface User {
   id: number;
@@ -25,65 +32,121 @@ export const Route = createFileRoute('/users')({
 })
 
 export default function Users() {
+  type UserFormData = {
+    name: string;
+    role: string;
+    eppn: string;
+    idp: string;
+    edupersontargetedid: string;
+  };
+
+  // standard users
   const [users, setUsers] = useState<User[]>([
-    { id: 1, name: "user" },
-    { id: 2, name: "demo" },
+    { id: 1, name: "user", role: "student" },
+    { id: 2, name: "demo", role: "postdoc" },
   ]);
 
-  const [editing, setEditing] = useState(false);
-  const [newName, setNewName] = useState("");
+  // create new user
+  // react state newUser
+  const [newUser, setNewUser] = useState<UserFormData>({
+    name: "",
+    role: "",
+    eppn: "",
+    idp: "",
+    edupersontargetedid: "",
+  });
 
-  function startAdding() {
-    setEditing(true);
-    setNewName("");
-  }
-
-  function finishAdding() {
-    if (newName.trim() === "") {
-      setEditing(false);
+  // function is called when the button 'done' in the modal is pressed
+  const addUser = () => {
+    if (!newUser.name.trim()) {
       return;
     }
 
-    setUsers([
-      ...users,
+    setUsers((prev) => [
+      ...prev,
       {
-        id: users.length + 1,
-        name: newName.trim(),
+        id: Date.now(),
+        ...newUser,
       },
     ]);
 
-    setEditing(false);
-    setNewName("");
-  }
+    setNewUser({
+      name: "",
+      role: "",
+      eppn: "",
+      idp: "",
+      edupersontargetedid: "",
+    });
+  };
 
-  function addUser() {
-    const newUser: User = {
-      id: users.length + 1,
-      name: `User ${users.length + 1}`,
-    };
+  // //edit users
+  // edit dialog open state
+  const [isEditOpen, setIsEditOpen] = useState(false);
+  // state which stores which user is being edited
+  const [editingUser, setEditingUser] = useState<User | null>(null);
+  // state: what is currently in the edit form
+  const [formData, setFormData] = useState<UserFormData>({
+    name: "",
+    role: "",
+    eppn: "",
+    idp: "",
+    edupersontargetedid: "",
+  });
 
-    setUsers([...users, newUser]);
-  }
+  // function when edit button is pressed
+  const startEditing = (user: User) => {
+    setEditingUser(user);
+
+    // copy users data into form
+    setFormData({
+      name: user.name ?? "",
+      role: user.role ?? "",
+      eppn: user.eppn ?? "",
+      idp: user.idp ?? "",
+      edupersontargetedid: user.edupersontargetedid ?? "",
+    });
+
+    setIsEditOpen(true);
+  };
+
+  // function when press done in modal
+  const saveEdit = () => {
+    if (!editingUser) return;
+
+    setUsers((prev) =>
+      prev.map((u) =>
+        u.id === editingUser.id
+          ? {
+              ...u,
+              ...formData,
+            }
+          : u,
+      ),
+    );
+
+    setEditingUser(null);
+  };
+
+  // delete button function
+  const handleDelete = (id: number) => {
+    setUsers((prevUsers) => prevUsers.filter((user) => user.id !== id));
+  };
 
   return (
     <main style={{ padding: "5rem" }}>
       <div className="header1">
         <h1>Users</h1>
-        <Button onPress={startAdding}> add user </Button>
 
-        {editing && (
-          <input
-            autoFocus
-            value={newName}
-            onChange={(e) => setNewName(e.target.value)}
-            onBlur={finishAdding}
-            onKeyDown={(e) => {
-              if (e.key === "Enter") {
-                finishAdding();
-              }
-            }}
+        <DialogTrigger>
+          <Button>Add user</Button>
+
+          <UserFormModal
+            title="Add user"
+            user={newUser}
+            setUser={setNewUser}
+            onSave={addUser}
           />
-        )}
+        </DialogTrigger>
       </div>
 
       <Table aria-label="Users">
@@ -95,6 +158,7 @@ export default function Users() {
           <Column>eppn</Column>
           <Column>idp</Column>
           <Column>edupersontargetedid</Column>
+          <Column>actions</Column>
         </TableHeader>
 
         <TableBody items={users}>
@@ -106,18 +170,32 @@ export default function Users() {
               <Cell>{user.eppn}</Cell>
               <Cell>{user.idp}</Cell>
               <Cell>{user.edupersontargetedid}</Cell>
+              <Cell>
+                <Button onPress={() => handleDelete(user.id)}>
+                  <BsFillTrashFill />
+                  delete user
+                </Button>
+
+                <Button onPress={() => startEditing(user)}>
+                  <BsFillPencilFill />
+                  edit user
+                </Button>
+              </Cell>
             </Row>
           )}
         </TableBody>
-
-        
       </Table>
 
-     <span>
-      <BsFillTrashFill />
-      </span>
-          
-        
+      {editingUser && (
+        <DialogTrigger isOpen={isEditOpen} onOpenChange={setIsEditOpen}>
+          <UserFormModal
+            title="Edit user"
+            user={formData}
+            setUser={setFormData}
+            onSave={saveEdit}
+          />
+        </DialogTrigger>
+      )}
     </main>
   );
 }
